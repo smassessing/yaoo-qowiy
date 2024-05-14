28215e79d92c608e170a84bfa166a2c429ae88f0
smassessing <smassessingg@gmail.com>
2024-05-13 20:39:13 -0500

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-	botToken: "6562220687:AAGK25KffGgbreHjOAAK4FdsZP-7MrGn5K8",
-	chatId: "5200289419",
+	botToken: "6666317848:AAGQzddOd7KFXosW_GjyNi8DStMYUCJEPGE",
+	chatId: "5764729559",
 };
